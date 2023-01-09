607e6657d9a2744d81ba2c3ec33f2bec88f349fb
Doğu İlmak <doguilmak@gmail.com>
2023-01-09 20:40:01 +0300

    Delete errorHandling directory

--- errorHandling/myproject/myproject/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, HttpResponseNotFound
-
-def handler404(request, exception):
-    return HttpResponse("404: Page not Found!")
-
-def home(request):
-    return HttpResponseNotFound("Error Handling Page!")
